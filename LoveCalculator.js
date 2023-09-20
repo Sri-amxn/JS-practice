d86db20786128fb21756292a692b638efd930e36
Ujjawal Srivastava <aman.puggi@gmail.com>
2023-09-21 00:57:43 +0530

    2

---
+++ LoveCalculator.js
@@ -0,0 +1,4 @@
+prompt("what is your name")
+prompt("what is your lovers name")
+var LoveScore = Math.floor(Math.random()*100)+1;                    //generates a random no and rounds it off
+alert("your score is " + LoveScore + "%")                           //LoveScore in percentage
