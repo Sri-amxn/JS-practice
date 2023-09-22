9138891aeeab04364f86ed6d6d159d5d48fda66e
Ujjawal Srivastava <aman.puggi@gmail.com>
2023-09-23 02:10:51 +0530

    updated

--- LoveCalculator.js
+++ LoveCalculator.js
@@ -1,4 +1,8 @@
 prompt("what is your name")
 prompt("what is your lovers name")
-var LoveScore = Math.floor(Math.random()*100)+1;                    //generates a random no and rounds it off
-alert("your score is " + LoveScore + "%")                           //LoveScore in percentage
+var LoveScore = Math.floor(Math.random()*100)+1;                                                    //generates a random no and rounds it off
+if (LoveScore > 70){
+    alert("your score is " + LoveScore + "% " + "you love each other like kanye love kanye")       //LoveScore in percentage
+} else{
+    alert("your score is " + LoveScore + "% ")
+}
